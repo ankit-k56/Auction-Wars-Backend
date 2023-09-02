c486abc219ee4ba0bcef9cbaee132f1588ba11e5
Ankit <ankitkumar19041@gmail.com>
2023-09-02 21:44:04 +0530

    image uploaded on firebase bucket

--- controllers/auction.js
+++ controllers/auction.js
@@ -2,10 +2,16 @@ const Auction = require('../models/Auction')
 const User = require('../models/User')
 const NotFoundError = require('../errors/not-found')
 const Badrequest = require('../errors/bad-request')
+const { initializeApp } = require("firebase/app");
+const { getStorage, ref, getDownloadURL, uploadBytesResumable } = require("firebase/storage");
+const config = require( "../config/firbase.config")
+const { v4: uuidv4 } = require('uuid');
 
 const fs = require('fs')
 const asyncErrorHandler = require('../utils/AsyncErrorHandler')
+initializeApp(config);
 const path = require('path')
+const storage = getStorage();
 
 
 require('dotenv').config()
@@ -28,17 +34,18 @@ const PostAuction = asyncErrorHandler(async(req, res)=>{
     const { title,description,  initialBid,  startDate, duration, bidders } = req.body;
     if(!title || !description || !initialBid || !startDate || !duration){
         throw new Badrequest('Not all required field provided in request!')
-        // next(err)
+        
     }
     const creater = req.user.id;
-    // const dir = path.join(__dirname,'../public/')
-    // const fl = fs.readdirSync(dir)
-    // console.log(fl.find((filename)=>{return filename == req.file.filename}))
-    // console.log(fl)
-    // console.log(req.file.filename)
 
+    const storageRef = ref(storage, `files/${uuidv4()}`);
+    const metadata = {
+        contentType: req.file.mimetype,
+    };
+    const snapshot = await uploadBytesResumable(storageRef, req.file.buffer, metadata);
+    const downloadURL = await getDownloadURL(snapshot.ref);
 
-    const auction = await Auction.create({title,description, photo:req.file.filename, initialBid, creater, startDate, duration,bidders})
+    const auction = await Auction.create({title,description, photo:downloadURL, initialBid, creater, startDate, duration,bidders})
     await User.findByIdAndUpdate(creater,{$push:{postedAuctions:auction._id}},{new:true})
     res.status(200).send({auction})
    
