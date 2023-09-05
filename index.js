5c1d015cef3555dcc76a2cf857b42c5a62cf7e74
Ankit <ankitkumar19041@gmail.com>
2023-09-05 23:28:30 +0530

    fixign cors origin

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const server = async()=>{
         })
         const io = SocketIo(sr,{
             cors:{
-                origin:"https://auction-wars.vercel.app/"
+                origin:"https://auction-wars.vercel.app"
             }
         });
         await io.on("connection", async(socket)=>{
